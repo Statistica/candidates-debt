41339073c67625e3fc84f70b98f2be5ddbedf71e
Jonathan Saewitz <busterroni11@gmail.com>
2016-05-30 22:44:58 -0400

    Update debt_calculations.py

--- debt_calculations.py
+++ debt_calculations.py
@@ -10,7 +10,7 @@ with open('campaigns_and_debt.csv', 'r') as f:
 	reader=csv.reader(f)
 	reader.next() #skip the header row
 	for row in reader: #loop through each row (candidate)
-		amount+=float(row[1]) #row[1] is the current candidate's amount
+		amount+=float(row[1]) #row[1] is the current candidate's debt
 		amounts.append(float(row[1]))
 
 print "# of candidates:", "{:,}".format(len(amounts))
